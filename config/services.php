401ddab2873ded6b7dc1368bf19b02564c2e2b79
Thonedra <thonedra.dev@gmail.com>
2026-05-19 15:46:32 +0800

    When we use stripe, we will get the stripe-sandbox testing mode keys from stripe official website, so, we need to embed those keys inside .env file, and define placeholders or variables in this services.php of those keys

--- config/services.php
+++ config/services.php
@@ -41,4 +41,10 @@ return [
     'redirect' => env('GOOGLE_REDIRECT_URI'),
 ],
 
+'stripe' => [
+    'key'            => env('STRIPE_PUBLISHABLE_KEY'),
+    'secret'         => env('STRIPE_SECRET_KEY'),
+    'webhook_secret' => env('STRIPE_WEBHOOK_SECRET', ''),
+],
+
 ];
