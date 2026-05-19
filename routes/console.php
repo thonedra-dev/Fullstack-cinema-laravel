f7fbf10d0cd0c4686a1d6f916e94f45a278890f6
Thonedra <thonedra.dev@gmail.com>
2026-05-19 15:51:23 +0800

    This console.php si the one that will operate the CleanExpiredBookings command controller from the app/ always at the background. So, the CleanExpiredBookings inside app/ is the definition, but this one is the real driver, and we need to run [ php artisan schedule:work ] always at the background, so, in our program, it will always check which seat in pending has exceeded the time given based on the expires_at in bookings table

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\CleanExpiredBookings;
+
+Schedule::command(CleanExpiredBookings::class)->everyMinute();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
